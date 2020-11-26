50d7a63e5649b8604e8faab568e4c127cf3b0643
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:21:00 +0200

    Dio looking for heaven again

---
+++ Car/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        class Car
+        {
+            string markName;
+            string modelName;
+            string registration;
+            string color;
+            int odometer;
+            int fuel;
+            int laps;
+
+
+            public Car(string _markName, string _modelName, string _color, string registration )
+            {
+                markName = _markName;
+                modelName = _modelName;
+                fuel = 60;
+                odometer = 0;
+                laps = 0;
+                Console.WriteLine($"The {modelName} model car has been created.");
+            }
+
+            public void ShowCarData()
+            {
+                Console.WriteLine($"Model name: {modelName}; Mark name: {markName}; Color: {color}; Odometer: {odometer}; Fuel tank: {fuel}; Regreistration: {registration}");
+            }
+
+            public void Lap()
+            {
+                laps += 1;
+                fuel -= 5;
+                odometer += 100;
+            }
+         
+            public double CompletedLaps
+            {
+                
+                get { return laps, fuel, odometer; }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("Mustang", "Fastback", "orange", "Jotaro");
+
+            Console.WriteLine($"Number of completed laps: {newCar.CompletedLaps}");
+
+            while (newCar.CompletedLaps != 0)
+            {
+                newCar.Lap();
+            }
+
+            Console.WriteLine($"Number of completed laps: {newCar.CompletedLaps}");
+
+        }
+    }
+}
---
+++ OPchallenges/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace OPchallenges
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} has been created.");
+
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Yare Yare Daze.");
+                happiness += 0.2;
+
+            }
+            public void Wiggle()
+            {
+                Console.WriteLine("Wiggle wiggle");
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happy: {happiness}; ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog("Koer", 10);
+            
+            Console.WriteLine($"The level of happiness: {newDog.Happiness}");
+
+            while(newDog.Happiness != 1)
+            {
+                newDog.Barks();
+            }
+
+            newDog.Wiggle();
+
+            Console.WriteLine($"The level of happiness: {newDog.Happiness}");
+
+            Console.WriteLine("Name the dog: ");
+            string newDogName = Console.ReadLine();
+            newDog.Rename(newDogName);
+            newDog.ShowDogData();
+        }
+    }
+}
